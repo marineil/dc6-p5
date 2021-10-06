3146e57e1208969b86876c1834933b6d19959bb1
marineil <marineilelle99@gmail.com>
2021-10-06 20:50:01 +0800

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { RndnumComponent } from './rndnum/rndnum.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RndnumComponent
   ],
   imports: [
     BrowserModule
